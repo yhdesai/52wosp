b4ecfa7a6da87c4975178c682b5a32f116ae8117
yash desai <yhdelectric@gmail.com>
2021-10-09 16:21:13 +0530

    :art: update color of github in navbar

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ export default function Home() {
           <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <span className="ml-3 text-xl">🚀 52 Weekends of Side Projects</span>
           </a>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-900" href="https://github.com/yhdesai/52wosp" target="_blank" rel="noreferrer noopener">Github</a>
+          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center text-gray-200">
+            <a class="mr-5 hover:text-gray-500" href="https://github.com/yhdesai/52wosp" target="_blank" rel="noreferrer noopener">Github</a>
           </nav>
           {/* <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
