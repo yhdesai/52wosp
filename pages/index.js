d1e2c143e0fe4f64dd308245e44c970e5368ca62
yash desai <yhdelectric@gmail.com>
2021-10-09 16:20:26 +0530

    :wrench: Added GitHub

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,21 @@ export default function Home() {
         <link rel="icon" href="/logo.png" />
       </Head>
 
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0">
+      
+
+      <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <span className="ml-3 text-xl">🚀 52 Weekends of Side Projects</span>
           </a>
+          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-gray-900" href="https://github.com/yhdesai/52wosp" target="_blank" rel="noreferrer noopener">Github</a>
+          </nav>
+          {/* <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button> */}
         </div>
       </header>
 
